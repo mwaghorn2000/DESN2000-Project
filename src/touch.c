a808fda0038d0ef1184a72bf60c36b737b0a83f3
joshualui582 <z5692724@ad.unsw.edu.au>
2026-08-12 10:34:44 +1000

    revert command bytes

--- src/touch.c
+++ src/touch.c
@@ -29,8 +29,8 @@ void touch_read_xy(char *x, char *y, int *pressure)
 	//Read Y co-ordinate from the touch screen controller
 	*y = touch_read(0x98);
 	
-	z1 = touch_read(0xB8);
-	z2 = touch_read(0xC8);
+	z1 = touch_read(0x78);
+	z2 = touch_read(0xB8);
 	if (z1 == 0) {
         *pressure = 0;       /* not touching */
     } else {
